fd79ff3e52dca2928e371433983a301b2c50646b
laur3nk <laurenk@berkeley.edu>
2025-11-03 17:29:36 -0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibGF1cmUzbmsiLCJhIjoiY21oOXJoMnd5MGU3ZDJqcHhqOGYxOGd1YSJ9.cyu6FHCrcG3ppE5UtYTT7w';
-const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/laure3nk/cmh9rputb000001sm3vwuf5c0', // your Style URL goes here
-  center: [-122.27, 37.87], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-  zoom: 9 // starting zoom
-    });
